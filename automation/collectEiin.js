8ec25e5eaf9cf6581ce8b335fd2099d6963c3ca9
Mahfuz Anam <94157542+mahfuz68@users.noreply.github.com>
2023-05-20 04:16:52 +0600

    Update collectEiin.js

--- automation/collectEiin.js
+++ automation/collectEiin.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 
 let count = 0;
 const scrape = async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.exposeFunction("saveToDB", saveToDB);
   await page.goto(
